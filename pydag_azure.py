96a74754cce5a3b2dc686f353410ebee167dbbf1
Marcos Soto <31476977+marcosdh1987@users.noreply.github.com>
2021-06-08 11:52:34 -0300

    Update pydag_azure.py
    data-month changed to use English format

--- pydag_azure.py
+++ pydag_azure.py
@@ -19,8 +19,8 @@ from datetime import datetime
 def read_time():
     # datetime object containing current date and time
     now = datetime.now()
-    # dd/mm/YY H:M:S
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    # mm/dd/YY H:M:S
+    dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
     return dt_string
 
 #database connection
